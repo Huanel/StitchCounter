5f00598fd507d7a28dc52aea9b8ed683c9b7d21b
Mar <regan.macneil@gmail.com>
2026-06-23 18:36:59 -0300

    Initial Commit

---
+++ StitchCounter/StitchCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StitchCounterApp.swift
+//  StitchCounter
+//
+//  Created by Marcia Orezzoli on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct StitchCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
